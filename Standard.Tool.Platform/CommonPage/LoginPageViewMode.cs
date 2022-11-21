99861428c9b981ff074aac62acd3feba044aff1b
yunqian44 <yunqian8@live.com>
2022-11-21 16:56:22 +0800

    login page Binding ViewModel

--- Standard.Tool.Platform/CommonPage/LoginPageViewMode.cs
+++ Standard.Tool.Platform/CommonPage/LoginPageViewMode.cs
@@ -1,13 +1,63 @@
-﻿using System;
+﻿using Standard.Tool.Platform.MVVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Standard.Tool.Platform.CommonPage
 {
-    public class LoginPageViewMode
+    public class LoginPageViewMode:ObservableObject
     {
 
+        #region 用户名
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                RaisePropertyChanged("UserName");
+            }
+        }
+        #endregion
+
+        #region 密码
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                RaisePropertyChanged("Password");
+            }
+        }
+        #endregion
+
+        public LoginPageViewMode()
+        {
+            
+        }
+
+
+        #region 登录操作
+        internal void SignInExecute()
+        {
+
+        }
+
+        internal bool CanSignInExecute()
+        {
+            return true;
+        }
+
+        internal ICommand SignIn
+        {
+            get { return new RelayCommand(SignInExecute, CanSignInExecute); }
+        } 
+        #endregion
     }
 }
