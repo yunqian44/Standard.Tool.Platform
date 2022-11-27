1eabb5a4eabd407acc359310372a6a8be6148963
yunqian44 <yunqian8@live.com>
2022-11-27 21:48:59 +0800

    Modify MainPage1 Name

---
+++ Standard.Tool.Platform/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Standard.Tool.Platform.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Standard.Tool.Platform
+{
+    public class MainWindowViewModel: ObservableObject
+    {
+        public MainWindowViewModel() 
+        {
+
+        }
+
+        void SignOutExecute(MainWindow window)
+        {
+            window.Close();
+        }
+
+        bool CanSignOutExecute(MainWindow window)
+        {
+            return true;
+        }
+
+        public ICommand SignOut
+        {
+            get { return new RelayCommand<MainWindow>(SignOutExecute, CanSignOutExecute); }
+        }
+    }
+}
