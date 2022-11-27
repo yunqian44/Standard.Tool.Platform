e55839edeab44664a2b80bb37143ca462932e333
yunqian44 <yunqian8@live.com>
2022-11-27 21:48:00 +0800

    UserControl---Menu

---
+++ Standard.Tool.Platform/UserControls/UserControlMenuItem.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Standard.Tool.Platform.MainWindow;
+
+namespace Standard.Tool.Platform.UserControls
+{
+    /// <summary>
+    /// UserControlMenuItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserControlMenuItem : UserControl
+    {
+        MainWindow _context;
+        public UserControlMenuItem(ItemMenu itemMenu, MainWindow context)
+        {
+            InitializeComponent();
+
+            _context = context;
+
+            ExpanderMenu.Visibility = itemMenu.SubItems == null ? Visibility.Collapsed : Visibility.Visible;
+            ListViewItemMenu.Visibility = itemMenu.SubItems == null ? Visibility.Visible : Visibility.Collapsed;
+
+            this.DataContext = itemMenu;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _context.SwitchScreen(((SubItem)((ListView)sender).SelectedItem).Screen);
+        }
+    }
+}
