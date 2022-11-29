129e445f6cb257fb338c5cd623571941d668f8ca
yunqian44 <yunqian8@live.com>
2022-11-29 18:41:04 +0800

    Json,Table Helper

---
+++ Standard.Tool.Platform.Common/Helper/TableDataHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Common.Helper
+{
+    public class TableDataHelper
+    {
+        static string contentRoot = string.Empty;
+
+        public TableDataHelper(string ContentRootPath)
+        {
+            contentRoot = ContentRootPath;
+        }
+
+        public static string GetData(string tableName)
+        {
+            string data = ReadData(Path.Combine(contentRoot, "TableData", $"{tableName}.json"), Encoding.UTF8);
+            return data;
+        }
+
+        private static string ReadData(string Path, Encoding encode)
+        {
+            string s = "";
+            try
+            {
+                if (!System.IO.File.Exists(Path))
+                {
+                    s = null;
+                }
+                else
+                {
+                    StreamReader f2 = new StreamReader(Path, encode);
+                    s = f2.ReadToEnd();
+                    f2.Close();
+                    f2.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                //LogLock.OutSql2Log("AOPLog", new string[] { dataIntercept });
+            }
+            return s;
+        }
+    }
+
+}
