0b2e7891b8f5f3e0b59af1b9214770a8952e0406
yunqian44 <yunqian8@live.com>
2022-12-11 17:06:26 +0800

    Account Domain Permission Domain

---
+++ Standard.Tool.Platform.Auth/Permission.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Auth
+{
+    public record Permission
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 权限名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 权限编码
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// 最后修改时间
+        /// </summary>
+        public DateTime? LastModifiedTimeUtc { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTimeUtc { get; set; }
+    }
+}
