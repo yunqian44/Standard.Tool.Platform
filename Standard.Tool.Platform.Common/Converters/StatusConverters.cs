ffe6f0da3b788c13903f086ff466c720114c9f61
yunqian44 <yunqian8@live.com>
2022-12-11 17:06:54 +0800

    DateTime & UserStatus Convert

---
+++ Standard.Tool.Platform.Common/Converters/StatusConverters.cs
@@ -0,0 +1,33 @@
+﻿using Standard.Tool.Platform.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Standard.Tool.Platform.Common.Converters
+{
+    public class AccountStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string sRet = "";
+            if (value != null)
+            {
+                sRet = value.ToString().ToLower() switch
+                {
+                    "enable" => string.Format("启用"),
+                    "disable" => string.Format("禁用"),
+                    _ => throw new NotFoundException("Invalide status types,")
+                };
+            }
+            return sRet;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
