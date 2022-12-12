9b47a8c87eaeb0e84f1f0e31b6c1b483cb2b149e
yunqian44 <yunqian8@live.com>
2022-12-12 21:08:54 +0800

    Add DbContext

---
+++ Standard.Tool.Platform.Data/Infrastructure/ISpecification.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Data.Infrastructure
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> Include { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+
+        int Take { get; }
+        int Skip { get; }
+        bool IsPagingEnabled { get; }
+    }
+}
