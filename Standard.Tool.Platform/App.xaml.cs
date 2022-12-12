ba990e9affc06651edc00e6e3e78b8e29dd56613
yunqian44 <yunqian8@live.com>
2022-12-12 21:15:52 +0800

    dependency Injection InitStartUp SericeProvider

--- Standard.Tool.Platform/App.xaml.cs
+++ Standard.Tool.Platform/App.xaml.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Standard.Tool.Platform.Common.Helper;
+using Standard.Tool.Platform.Extension;
+using Standard.Tool.Platform.Pages.Account;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using Standard.Tool.Platform.Data.Infrastructure;
+using Standard.Tool.Platform.Auth;
+using Standard.Tool.Platform.PostgreSql;
+using Standard.Tool.Platform.Data.Infrastructure.Services;
+using MediatR;
 
 namespace Standard.Tool.Platform
 {
@@ -19,24 +29,48 @@ namespace Standard.Tool.Platform
     /// </summary>
     public partial class App : Application
     {
-        public static IServiceProvider ServiceProvider;
+
+        public static readonly string _connStr;
         protected async override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
             var hostbuilder = CreateHostBuilder(e.Args);
             var host = await hostbuilder.StartAsync();
-            ServiceProvider = host.Services;
+            ProviderFactory.ServiceProvider = host.Services;
+            await host.InitStartUp();
             host.Services.GetRequiredService<MainWindow>()?.Show();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+            AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Materials");
+            AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Menus");
+            AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Data");
+
+            var configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).Add(new JsonConfigurationSource { Path = $"appsettings.json", Optional = false, ReloadOnChange = true }).Build();
+            var connStr = configuration.GetConnectionString("Standard.Tool.Platform.Database");
+
             var hostBuilder = Host.CreateDefaultBuilder(args);
             hostBuilder.ConfigureServices((ctx, services) =>
             {
+                services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+                services.AddTransient<IReadTableService, ReadTableService>();
                 services.AddSingleton(new TableDataHelper(AppDomain.CurrentDomain.BaseDirectory));
+                services.AddPostgreSqlStorage(connStr);
+
                 services.AddSingleton<MainWindow>();
+                services.AddSingleton<AccountPage>();
+
+
+
+
+                services.AddTransient<Account>();
+
+
+
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
             });
 
             return hostBuilder;
---
+++ Standard.Tool.Platform/ApplicationExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Standard.Tool.Platform.Data;
+using Standard.Tool.Platform.PostgreSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform;
+
+public static class ApplicationExtensions
+{
+    public static async Task<StartupInitResult> InitStartUp(this IHost app)
+    {
+        var services = app.Services;
+
+        var context = services.GetRequiredService<PostgreSqlToolsBlockDbContext>();
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+
+        var logger = loggerFactory.CreateLogger<App>();
+        bool canConnect = await context.Database.CanConnectAsync();
+        if (!canConnect) return StartupInitResult.DatabaseConnectionFail;
+
+        await context.Database.EnsureCreatedAsync();
+
+
+        bool isNew = !await context.Account.AnyAsync();
+        if (isNew)
+        {
+            try
+            {
+                logger.LogInformation("Seeding database...");
+
+                await context.ClearAllData();
+                await Seed.SeedAsync(context, logger);
+
+                logger.LogInformation("Database seeding successfully.");
+
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical(e, e.Message);
+                return StartupInitResult.DatabaseSetupFail;
+            }
+        }
+
+        return StartupInitResult.None;
+    }
+}
+
+public enum StartupInitResult
+{
+    None = 0,
+    DatabaseConnectionFail = 1,
+    DatabaseSetupFail = 2
+}
