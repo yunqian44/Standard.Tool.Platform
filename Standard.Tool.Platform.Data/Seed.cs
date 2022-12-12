f8f9aef74b1a5138f1f84ef01ce98ec5e8483ab9
yunqian44 <yunqian8@live.com>
2022-12-12 21:14:09 +0800

    Add User Seed Data

---
+++ Standard.Tool.Platform.Data/Seed.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using Standard.Tool.Platform.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Data;
+
+public class Seed
+{
+    public static async Task SeedAsync(ToolsBlockDbContext dbContext, ILogger logger, int retry = 0)
+    {
+        var retryForAvailability = retry;
+
+        try
+        {
+            await dbContext.Account.AddRangeAsync(GetAccounts());
+
+            await dbContext.SaveChangesAsync();
+        }
+        catch (Exception e)
+        {
+            if (retryForAvailability >= 10) throw;
+
+            retryForAvailability++;
+
+            logger.LogError(e.Message);
+            await SeedAsync(dbContext, logger, retryForAvailability);
+            throw;
+        }
+    }
+
+    private static IEnumerable<AccountEntity> GetAccounts()
+    {
+        return new List<AccountEntity>
+    {
+        new()
+        {
+            Id = Guid.Parse("ab78493d-7569-42d2-ae78-c2b610ada1aa"),
+            UserName = "admin",
+            PasswordHash = "JAvlGPq9JyTdtvBO6x2llnRI1+gxwIyPqCKAn3THIKk=",
+            Status="Enable",
+            CreateTimeUtc = DateTime.UtcNow
+        }
+    };
+    }
+}
