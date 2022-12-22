f7abbae2b58e603a6ede5fd8f09dab2af3747f25
yunqian44 <yunqian8@live.com>
2022-12-22 22:24:38 +0800

    Add Permission SeedData

--- Standard.Tool.Platform.Data/ToolsBlockDbContext.cs
+++ Standard.Tool.Platform.Data/ToolsBlockDbContext.cs
@@ -20,15 +20,34 @@ namespace Standard.Tool.Platform.Data
         }
 
         public virtual DbSet<MenuEntity> Menu { get; set; }
+
         public virtual DbSet<SubMenuEntity> SubMenu { get; set; }
 
+        public virtual DbSet<AccountPermissionEntity> AccountPermission { get; set; }
+
         public virtual DbSet<AccountEntity> Account { get; set; }
+
+        public virtual DbSet<PermissionEntity> Permission { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new MenuConfiguration());
             modelBuilder.ApplyConfiguration(new SubMenuConfiguration());
-          
+
+
+            modelBuilder.Entity<AccountPermissionEntity>()
+           .HasKey(t => new { t.UserId, t.PermissionId });
+
+            modelBuilder.Entity<AccountPermissionEntity>()
+                .HasOne(pt => pt.Account)
+                .WithMany(p => p.AccountPermissions)
+                .HasForeignKey(pt => pt.UserId);
+
+            modelBuilder.Entity<AccountPermissionEntity>()
+                .HasOne(pt => pt.Permission)
+                .WithMany(t => t.AccountPermissions)
+                .HasForeignKey(pt => pt.PermissionId);
+
         }
     }
 
@@ -36,6 +55,7 @@ namespace Standard.Tool.Platform.Data
     {
         public static async Task ClearAllData(this ToolsBlockDbContext context)
         {
+            context.AccountPermission.RemoveRange();
             context.Account.RemoveRange();
             await context.SaveChangesAsync();
         }
