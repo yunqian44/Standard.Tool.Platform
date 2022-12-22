44205a0e667e7077511be1ec8aebedbf260e9f90
yunqian44 <yunqian8@live.com>
2022-12-22 22:25:01 +0800

    Permission Domain

---
+++ Standard.Tool.Platform.Auth/PermissionFeature/GetPermissionsQuery.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Standard.Tool.Platform.Data.Entities;
+using Standard.Tool.Platform.Data.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Standard.Tool.Platform.Common.Helper;
+
+
+namespace Standard.Tool.Platform.Auth.PermissionFeature;
+
+public record GetPermissionsQuery : IRequest<IList<Permission>>;
+
+public class GetPermissionsQueryHandler : IRequestHandler<GetPermissionsQuery, IList<Permission>>
+{
+    private readonly IRepository<PermissionEntity> _repo;
+
+    public GetPermissionsQueryHandler(IRepository<PermissionEntity> repo) => _repo = repo;
+
+    public async Task<IList<Permission>> Handle(GetPermissionsQuery request, CancellationToken ct)
+    {
+        return await _repo.SelectAsync(p => new Permission
+        {
+            Id = p.Id,
+            CreateTimeUtc = p.CreateTimeUtc,
+            Code=p.Code,
+            Name=p.Name,
+            TypeName =p.Type.GetDescription(),
+            Status = p.Status,
+            No = 0
+        }, ct);
+    }
+}
---
+++ Standard.Tool.Platform.Auth/PermissionFeature/Permission.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Auth.PermissionFeature
+{
+    public class Permission
+    {
+        private int _no;
+        /// <summary>
+        /// 序号
+        /// </summary>
+        public int No
+        {
+            get { return _no; }
+            set
+            {
+                _no = ++value;
+            }
+        }
+
+        /// <summary>
+        /// 是否选中
+        /// </summary>
+        public int IsSelected { get; set; }
+
+        /// <summary>
+        /// GUID
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 权限名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 权限编码
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 模块名称
+        /// </summary>
+        public string TypeName { get; set; }
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// 最后修改时间
+        /// </summary>
+        public DateTime? LastModifiedTimeUtc { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreateTimeUtc { get; set; }
+    }
+}
