4e1bf0a715d42cc91324f40c596b931b7ada863f
yunqian44 <yunqian8@live.com>
2022-12-22 22:26:18 +0800

    Main Page Css

--- Standard.Tool.Platform/App.xaml.cs
+++ Standard.Tool.Platform/App.xaml.cs
@@ -1,79 +1,71 @@
 ﻿using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Standard.Tool.Platform.Common.Helper;
 using Standard.Tool.Platform.Extension;
 using Standard.Tool.Platform.Pages.Account;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Globalization;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using Standard.Tool.Platform.Data.Infrastructure;
-using Standard.Tool.Platform.Auth;
 using Standard.Tool.Platform.PostgreSql;
 using Standard.Tool.Platform.Data.Infrastructure.Services;
 using MediatR;
 using Standard.Tool.Platform.CommonPage;
+using Standard.Tool.Platform.Auth.AccountFeature;
+using Standard.Tool.Platform.Auth.PermissionFeature;
 
-namespace Standard.Tool.Platform
+namespace Standard.Tool.Platform;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public static readonly string _connStr;
+    protected async override void OnStartup(StartupEventArgs e)
     {
-        public static readonly string _connStr;
-        protected async override void OnStartup(StartupEventArgs e)
+        base.OnStartup(e);
+
+        var hostbuilder = CreateHostBuilder(e.Args);
+        var host = await hostbuilder.StartAsync();
+        ProviderFactory.ServiceProvider = host.Services;
+        await host.InitStartUp();
+        host.Services.GetRequiredService<MainWindow>()?.Show();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Materials");
+        AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Menus");
+        AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Data");
+
+        var configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).Add(new JsonConfigurationSource { Path = $"appsettings.json", Optional = false, ReloadOnChange = true }).Build();
+        var connStr = configuration.GetConnectionString("Standard.Tool.Platform.Database");
+
+        var hostBuilder = Host.CreateDefaultBuilder(args);
+        hostBuilder.ConfigureServices((ctx, services) =>
         {
-            base.OnStartup(e);
+            services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddTransient<IReadTableService, ReadTableService>();
+            services.AddSingleton(new TableDataHelper(AppDomain.CurrentDomain.BaseDirectory));
+            services.AddPostgreSqlStorage(connStr);
 
-            var hostbuilder = CreateHostBuilder(e.Args);
-            var host = await hostbuilder.StartAsync();
-            ProviderFactory.ServiceProvider = host.Services;
-            await host.InitStartUp();
-            host.Services.GetRequiredService<LoginPage>()?.Show();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Materials");
-            AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Menus");
-            AppDomain.CurrentDomain.Load("Standard.Tool.Platform.Data");
-
-            var configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).Add(new JsonConfigurationSource { Path = $"appsettings.json", Optional = false, ReloadOnChange = true }).Build();
-            var connStr = configuration.GetConnectionString("Standard.Tool.Platform.Database");
-
-            var hostBuilder = Host.CreateDefaultBuilder(args);
-            hostBuilder.ConfigureServices((ctx, services) =>
-            {
-                services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
-                services.AddTransient<IReadTableService, ReadTableService>();
-                services.AddSingleton(new TableDataHelper(AppDomain.CurrentDomain.BaseDirectory));
-                services.AddPostgreSqlStorage(connStr);
-
-                services.AddSingleton<MainWindow>();
-                services.AddSingleton<AccountPage>();
-                services.AddSingleton<LoginPage>();
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<AccountPage>();
+            services.AddSingleton<LoginPage>();
+            services.AddSingleton<PermissionPage>();
 
 
+            services.AddTransient<Account>();
+            services.AddTransient<Permission>();
 
-                services.AddTransient<Account>();
 
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
+        });
 
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            });
-
-            return hostBuilder;
-        }
+        return hostBuilder;
     }
 }
