d9f5fbd32a870355d2e3b18508995e8f22d3d0bf
yunqian44 <yunqian8@live.com>
2023-01-03 17:11:07 +0800

    Add MaterialOperatePage Page

---
+++ Standard.Tool.Platform/Pages/Project.Application/Material/MaterialDataOperatePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Standard.Tool.Platform.Data.Infrastructure;
+using System.Windows.Controls;
+
+namespace Standard.Tool.Platform.Pages.Project.Application.Material;
+
+/// <summary>
+/// MaterialDataOperatePage.xaml 的交互逻辑
+/// </summary>
+public partial class MaterialDataOperatePage : Page
+{
+    public MaterialDataOperatePage(IMediator mediator, IReadTableService readTableService)
+    {
+        InitializeComponent();
+        DataContext = new MaterialDataOperatePageViewModel(mediator, readTableService);
+    }
+}
