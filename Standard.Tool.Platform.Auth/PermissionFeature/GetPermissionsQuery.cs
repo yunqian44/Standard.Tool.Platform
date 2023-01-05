d048124f986beb950ca20e41a93a4b1f5fd3518c
yunqian44 <yunqian8@live.com>
2023-01-05 23:43:28 +0800

    PermissionQuery‘s Id ToString()

--- Standard.Tool.Platform.Auth/PermissionFeature/GetPermissionsQuery.cs
+++ Standard.Tool.Platform.Auth/PermissionFeature/GetPermissionsQuery.cs
@@ -1,13 +1,15 @@
 ﻿using MediatR;
+using NPOI.POIFS.Properties;
+using Standard.Tool.Platform.Common.Helper;
 using Standard.Tool.Platform.Data.Entities;
 using Standard.Tool.Platform.Data.Infrastructure;
+using Standard.Tool.Platform.Data.Spec;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using System.Security;
 using System.Threading;
 using System.Threading.Tasks;
-using Standard.Tool.Platform.Common.Helper;
-using System.Linq;
 
 namespace Standard.Tool.Platform.Auth.PermissionFeature;
 
@@ -21,28 +23,28 @@ public class GetPermissionsQueryHandler : IRequestHandler<GetPermissionsQuery, I
 
     public async Task<IList<Permission>> Handle(GetPermissionsQuery request, CancellationToken ct)
     {
-        return await _repo.SelectAsync(p => new Permission
+       return  await _repo.SelectAsync(p => new Permission
         {
-            Id = p.Id,
+            Id = p.Id.ToString().Trim(),
             CreateTimeUtc = p.CreateTimeUtc,
-            Code=p.Code,
-            Name=p.Name,
-            Type=p.Type,
-            TypeName =p.Type.GetDescription(),
-            ParentId=p.ParentId,
+            Code = p.Code,
+            Name = p.Name,
+            Type = p.Type,
+            TypeName = p.Type.GetDescription(),
+            ParentId = p.ParentId,
             Status = p.Status,
             No = 0,
             Parent = new Permission(p.Parent),
-            LastModifiedTimeUtc=p.LastModifiedTimeUtc,
-            Childrens= p.Childrens.Select(sm => new Permission
+            LastModifiedTimeUtc = p.LastModifiedTimeUtc,
+            Childrens = p.Childrens.Select(sm => new Permission
             {
-                Id=sm.Id,
-                Code=sm.Code,
-                Name=sm.Name,
-                Type=sm.Type,
-                TypeName=sm.Type.GetDescription(),
-                ParentId=sm.ParentId,
-                Status=sm.Status,
+                Id = sm.Id.ToString().Trim(),
+                Code = sm.Code,
+                Name = sm.Name,
+                Type = sm.Type,
+                TypeName = sm.Type.GetDescription(),
+                ParentId = sm.ParentId,
+                Status = sm.Status,
                 CreateTimeUtc = sm.CreateTimeUtc,
                 LastModifiedTimeUtc = sm.LastModifiedTimeUtc,
             }).ToList()
