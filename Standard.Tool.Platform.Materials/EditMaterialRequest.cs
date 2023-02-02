14c7b077b34e223aea6ac6af7bac78626b52de32
yunqian44 <yunqian8@live.com>
2023-02-02 22:00:24 +0800

    Using

--- Standard.Tool.Platform.Materials/EditMaterialRequest.cs
+++ Standard.Tool.Platform.Materials/EditMaterialRequest.cs
@@ -1,33 +1,24 @@
 ﻿using FluentValidation.Results;
-using Microsoft.EntityFrameworkCore.Query;
-using Standard.Tool.Platform.Data.Entities;
 using Standard.Tool.Platform.Materials.Validations;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Standard.Tool.Platform.Materials
+namespace Standard.Tool.Platform.Materials;
+
+public class EditMaterialRequest
 {
-    public class EditMaterialRequest
+    public EditMaterialRequest(IEnumerable<Material> materialDataList) 
     {
-        public EditMaterialRequest(IEnumerable<Material> materialDataList) 
-        {
-            MaterialDataList = materialDataList;
-        }
+        MaterialDataList = materialDataList;
+    }
 
-        public IEnumerable<Material> MaterialDataList { get; private set; }
+    public IEnumerable<Material> MaterialDataList { get; private set; }
 
 
-        public ValidationResult ValidationResult { get; set; }
+    public ValidationResult ValidationResult { get; set; }
 
-        public bool IsValid()
-        {
-            ValidationResult = new CreateMaterialValidation().Validate(this);
-            return ValidationResult.IsValid;
-        }
+    public bool IsValid()
+    {
+        ValidationResult = new CreateMaterialValidation().Validate(this);
+        return ValidationResult.IsValid;
     }
 }
