6aad465bdcb9c853015c6ddc0c38b5bb747c964a
yunqian44 <yunqian8@live.com>
2023-02-02 22:00:08 +0800

    AccountPermission Validate

---
+++ Standard.Tool.Platform.Auth/PermissionFeature/Validations/UpdateAccountPermissionValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Auth.PermissionFeature.Validations
+{
+
+    public class UpdateAccountPermissionValidation : AccountPermissionValidation<EditAccountPermissionRequest>
+    {
+        public UpdateAccountPermissionValidation()
+        {
+            ValidateCollectionCount();
+        }
+    }
+}
