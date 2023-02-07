dd01baf30e79c6ab22030480d0d221622a22dc49
yunqian44 <yunqian8@live.com>
2023-02-08 01:08:50 +0800

    Fix ChecBox Bug

--- Standard.Tool.Platform/Pages/Sys.Functions/Account/AssignUserPermissionPageViewModel.cs
+++ Standard.Tool.Platform/Pages/Sys.Functions/Account/AssignUserPermissionPageViewModel.cs
@@ -18,6 +18,7 @@ using Standard.Tool.Platform.Data.Entities;
 using NPOI.SS.Formula.Functions;
 using Standard.Tool.Platform.Materials;
 using NPOI.HSSF.Record.PivotTable;
+using Microsoft.VisualBasic;
 
 namespace Standard.Tool.Platform.Pages.Account
 {
@@ -102,57 +103,7 @@ namespace Standard.Tool.Platform.Pages.Account
             await Task.Run(async
                  () =>
              {
-
-                 if (ShowPermissionList == null) return;
-                 ShowPermissionList.ToList().ForEach(a =>
-                 {
-                     //Page Levels
-                     PermissionList.ToList().ForEach(b =>
-                     {
-                         if (b.Id == a.Id)
-                             b.IsSelected = a.IsSelected;
-                     });
-
-                     if (!a.IsSelected && AllPermissionList.Any(u=>u.Id==a.Id))
-                     {
-                         AllPermissionList.RemoveAt(Array.IndexOf(AllPermissionList.ToArray(), AllPermissionList.ToList().Find(p => p.Id == a.Id)));
-                     }
-                     else if (a.IsSelected && !AllPermissionList.Any(u => u.Id == a.Id))
-                     {
-                         AllPermissionList.Add(a);
-                     }
-
-
-                     //Control Levels
-                     if (a.Childrens is { Length: > 0 })
-                     {
-                         a.Childrens.ToList().ForEach(c =>
-                         {
-                             PermissionList.ToList().ForEach(d =>
-                             {
-                                 if (d.Childrens is { Length: > 0 })
-                                 {
-                                     d.Childrens.ToList().ForEach(e =>
-                                     {
-                                         if (c.Id == e.Id)
-                                             e.IsSelected = c.IsSelected;
-
-                                         if (!e.IsSelected&& AllPermissionList.Any(u=>u.Id==e.Id))
-                                         {
-                                             AllPermissionList.RemoveAt(Array.IndexOf(AllPermissionList.ToArray(), AllPermissionList.ToList().Find(p=>p.Id==e.Id)));
-                                         }
-                                         else if (e.IsSelected && !AllPermissionList.Any(u=> u.Id == e.Id))
-                                         {
-                                             AllPermissionList.Add(e);
-                                         }
-                                     });
-                                 }
-
-                             });
-                         });
-                     }
-                 });
-
+                 await Refresh();
 
                  ShowPermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(PermissionList.Where(u => u.ParentId == Guid.Parse(permissionId)));
 
@@ -180,21 +131,97 @@ namespace Standard.Tool.Platform.Pages.Account
         {
             get { return new RelayCommand<string>(SelectModuleExecute, CanSelectModuleExecute); }
         }
+
+        async Task Refresh()
+        {
+            await Task.Run(async
+                 () =>
+            {
+
+                if (ShowPermissionList == null) return;
+                ShowPermissionList.ToList().ForEach(a =>
+                {
+                    //Page Levels
+                    PermissionList.ToList().ForEach(b =>
+                    {
+                        if (b.Id == a.Id)
+                            b.IsSelected = a.IsSelected;
+                    });
+
+                    if (!a.IsSelected && AllPermissionList.Any(u => u.Id == a.Id))
+                    {
+                        AllPermissionList.RemoveAt(Array.IndexOf(AllPermissionList.ToArray(), AllPermissionList.ToList().Find(p => p.Id == a.Id)));
+                    }
+                    else if (a.IsSelected)
+                    {
+                        if (AllPermissionList.Any(u => u.Id == a.Id))
+                        {
+                            AllPermissionList.ToList().ForEach(c =>
+                            {
+                                if (c.Id == a.Id)
+                                    c.IsSelected = a.IsSelected;
+                            });
+                        }
+                        else
+                            AllPermissionList.Add(a);
+
+                    }
+
+
+                    //Control Levels
+                    if (a.Childrens is { Length: > 0 })
+                    {
+                        a.Childrens.ToList().ForEach(c =>
+                        {
+                            PermissionList.ToList().ForEach(d =>
+                            {
+                                if (d.Childrens is { Length: > 0 })
+                                {
+                                    d.Childrens.ToList().ForEach(e =>
+                                    {
+                                        if (c.Id == e.Id)
+                                            e.IsSelected = c.IsSelected;
+
+                                        if (!e.IsSelected && AllPermissionList.Any(u => u.Id == e.Id))
+                                        {
+                                            AllPermissionList.RemoveAt(Array.IndexOf(AllPermissionList.ToArray(), AllPermissionList.ToList().Find(p => p.Id == e.Id)));
+                                        }
+                                        else if (e.IsSelected)
+                                        {
+                                            if (AllPermissionList.Any(u => u.Id == e.Id))
+                                            {
+                                                AllPermissionList.ToList().ForEach(w =>
+                                                {
+                                                    if (w.Id == e.Id)
+                                                        w.IsSelected = e.IsSelected;
+                                                });
+                                            }
+                                            else
+                                                AllPermissionList.Add(e);
+                                        }
+                                    });
+                                }
+                            });
+                        });
+                    }
+                });
+            });
+        }
         #endregion
 
         #region 02，Search
         async void SearchExecute()
         {
             IList<AuthCore.PermissionFeature.Permission> dataList = null;
-           await Task.Run(async
-                () =>
-            {
-                dataList = await _mediator.Send(new GetPermissionsQuery());
+            await Task.Run(async
+                 () =>
+             {
+                 dataList = await _mediator.Send(new GetPermissionsQuery());
 
-                if (dataList == null) return;
-                ModuleList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(dataList.Where(u => u.ParentId == null));
-                PermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(dataList);
-            });
+                 if (dataList == null) return;
+                 ModuleList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(dataList.Where(u => u.ParentId == null));
+                 PermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(dataList);
+             });
         }
 
         bool CanSearchExecute()
@@ -228,56 +255,65 @@ namespace Standard.Tool.Platform.Pages.Account
         #region 04，AccountPermission
         public async void QueryAccountById(string userId)
         {
-           await Task.Run(async
-                () =>
-            {
-                Account = await _mediator.Send(new GetAccountByIdQuery(Guid.Parse(userId)));
-                var permissionList = await _mediator.Send(new GetPermissionsQuery());
+            await Task.Run(async
+                 () =>
+             {
+                 Account = await _mediator.Send(new GetAccountByIdQuery(Guid.Parse(userId)));
+                 var permissionList = await _mediator.Send(new GetPermissionsQuery());
 
-                AllPermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(permissionList);
-                ModuleList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(permissionList.Where(u => u.ParentId == null));
-                ShowPermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(permissionList.Where(u => u.ParentId != null && u.Type == PermissionType.Page));
+                 AllPermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(permissionList);
+                 ModuleList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(permissionList.Where(u => u.ParentId == null));
+                 ShowPermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(permissionList.Where(u => u.ParentId != null && u.Type == PermissionType.Page));
 
-                //Judge Page CheckBox and Control CheckBox Whether IsChecked
-                if (ShowPermissionList == null) return;
-                if (Account.Permissions is { Length: > 0 })
-                {
-                    foreach (var permission in Account.Permissions)
-                    {
-                        //Page Levels
-                        if (ShowPermissionList.Any(u => u.Id == permission.Id))
-                            ShowPermissionList.ToList().ForEach(a => { a.IsSelected = true; });
+                 //Judge Page CheckBox and Control CheckBox Whether IsChecked
+                 if (ShowPermissionList == null) return;
+                 if (Account.Permissions is { Length: > 0 })
+                 {
+                     foreach (var permission in Account.Permissions)
+                     {
+                         //Page Levels
+                         if (ShowPermissionList.Any(u => u.Id == permission.Id))
+                             ShowPermissionList.ToList().ForEach(a => {
+                                 if (a.Id == permission.Id)
+                                     a.IsSelected = true;
+                             });
 
-                        //Control Levels
-                        ShowPermissionList.ToList().ForEach(a =>
-                        {
-                            if (a.Childrens is { Length: > 0 })
-                            {
-                                if (a.Childrens.Any(u => u.Id == permission.Id))
-                                    a.Childrens.ToList().ForEach(b => { b.IsSelected = true; });
-                            }
-                        });
-                    }
-                }
+                         //Control Levels
+                         ShowPermissionList.ToList().ForEach(a =>
+                         {
+                             if (a.Childrens is { Length: > 0 })
+                             {
+                                 if (a.Childrens.Any(u => u.Id == permission.Id))
+                                     a.Childrens.ToList().ForEach(b => { 
+                                         if(b.Id==permission.Id)
+                                            b.IsSelected = true; 
+                                     });
+                             }
+                         });
+                     }
+                 }
 
-                PermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(ShowPermissionList);
-            });
+                 PermissionList = new ObservableCollection<AuthCore.PermissionFeature.Permission>(ShowPermissionList);
+             });
         }
         #endregion
 
         #region 05，Save
-        public void SaveExecute()
+        public async void SaveExecute()
         {
             // to do
-            var ids = AllPermissionList.Select(u => Guid.Parse(u.Id)).ToArray();
-            var request = new EditAccountPermissionRequest(ids);
-            if (request.IsValid())
+           await Task.Run(async () =>
             {
-                Task.Run(async () =>
+                await Refresh();
+                var ids =
+                    AllPermissionList.Where(u => u.ParentId != null && u.IsSelected).Select(u => Guid.Parse(u.Id)).ToArray();
+                var request = new EditAccountPermissionRequest(ids);
+                if (request.IsValid())
                 {
                     var exportResult = await _mediator.Send(new UpdateAccountCommand(Guid.Parse(Account.Id), request));
-                });
-            }
+
+                }
+            });
         }
 
         public bool CanSaveExecute()
