6d76a1b99f2d8c03e223f9b7a9420973422e928e
yunqian44 <yunqian8@live.com>
2023-03-20 22:12:42 +0800

    Menu SeedData

--- Standard.Tool.Platform.Data/Seed.cs
+++ Standard.Tool.Platform.Data/Seed.cs
@@ -205,7 +205,6 @@ public class Seed
     };
     }
 
-
     private static IEnumerable<AccountPermissionEntity> GetAccountPermissions()
     {
         return new List<AccountPermissionEntity>
@@ -217,7 +216,7 @@ public class Seed
         },
         new()
         {
-             AccountId=Guid.Parse("65A50B1F-CB16-4D03-9081-9297BA4AC497"),
+           AccountId=Guid.Parse("65A50B1F-CB16-4D03-9081-9297BA4AC497"),
            PermissionId=Guid.Parse("4904BB93-7566-4DB5-A8C4-37195F6BB89A")//权限信息页面
         },
         new()
@@ -256,5 +255,260 @@ public class Seed
             PermissionId = Guid.Parse("BEF549AE-CF14-45D6-B436-F8FED891AFC2")//物料导入导出-导入
         }
     };
+    }
+
+    private static IEnumerable<MenuEntity> GetMenus()
+    {
+        return new List<MenuEntity>
+        {
+            new()
+            {
+                Id = Guid.Parse("698C6F9D-DA83-4F63-B58A-6E5B76B069B3"),
+                DisplayOrder = 1,
+                Icon = "2748",
+                Title = "标准库导入导出",
+                SubMenus=new List<SubMenuEntity>
+                {
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=1,
+                        MenuId=Guid.NewGuid(),
+                        Name="标准脚本导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=2,
+                        MenuId=Guid.NewGuid(),
+                        Name="标准脚本导出",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=3,
+                        MenuId=Guid.NewGuid(),
+                        Name="标准设备导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=4,
+                        MenuId=Guid.NewGuid(),
+                        Name="标准设备导出",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=5,
+                        MenuId=Guid.NewGuid(),
+                        Name="标准PI导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=6,
+                        MenuId=Guid.NewGuid(),
+                        Name="标准PI导出",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=7,
+                        MenuId=Guid.NewGuid(),
+                        Name="数据库对象导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=8,
+                        MenuId=Guid.NewGuid(),
+                        Name="数据库对象导出",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=9,
+                        MenuId=Guid.NewGuid(),
+                        Name="XFP参数导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=10,
+                        MenuId=Guid.NewGuid(),
+                        Name="XFP参数导出",
+                        ScreenName=string.Empty
+                    }
+                }
+            },
+            new()
+            {
+                Id = Guid.Parse("0FBEC1D6-5ADC-491F-BF20-E10656CC60B9"),
+                DisplayOrder = 2,
+                Icon = "1809",
+                Title = "项目级应用功能",
+                SubMenus=new List<SubMenuEntity>
+                {
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=1,
+                        MenuId=Guid.NewGuid(),
+                        Name="用户批量导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=2,
+                        MenuId=Guid.NewGuid(),
+                        Name="客制化权限导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=3,
+                        MenuId=Guid.NewGuid(),
+                        Name="物料主数据导入",
+                        ScreenName="MaterialDataOperatePage"
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=4,
+                        MenuId=Guid.NewGuid(),
+                        Name="物料清单导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=5,
+                        MenuId=Guid.NewGuid(),
+                        Name="位置导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=6,
+                        MenuId=Guid.NewGuid(),
+                        Name="设备数据导入",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=7,
+                        MenuId=Guid.NewGuid(),
+                        Name="参数管理功能",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=8,
+                        MenuId=Guid.NewGuid(),
+                        Name="数据库对象导出",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=9,
+                        MenuId=Guid.NewGuid(),
+                        Name="打印机配置信息迁移",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=10,
+                        MenuId=Guid.NewGuid(),
+                        Name="称配信息迁移",
+                        ScreenName=string.Empty
+                    }
+                }
+            },
+            new()
+            {
+                Id = Guid.Parse("DF94A355-3C3F-405A-BF72-3779AD5550FF"),
+                DisplayOrder = 2,
+                Icon = "2718",
+                Title = "其他功能",
+                SubMenus=new List<SubMenuEntity>
+                {
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=1,
+                        MenuId=Guid.NewGuid(),
+                        Name="系统日志在线查看",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=2,
+                        MenuId=Guid.NewGuid(),
+                        Name="数据源配置",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=3,
+                        MenuId=Guid.NewGuid(),
+                        Name="翻译修改",
+                        ScreenName=string.Empty
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=4,
+                        MenuId=Guid.NewGuid(),
+                        Name="个人信息设置",
+                        ScreenName=string.Empty
+                    }
+                }
+            },
+            new()
+            {
+                Id = Guid.Parse("B071577A-4542-4644-8FA5-DF956A4AD9BF"),
+                DisplayOrder = 2,
+                Icon = "7103",
+                Title = "工具管理",
+                SubMenus=new List<SubMenuEntity>
+                {
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=1,
+                        MenuId=Guid.NewGuid(),
+                        Name="用户管理",
+                        ScreenName="AccountPage"
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                        DisplayOrder=2,
+                        MenuId=Guid.NewGuid(),
+                        Name="权限管理",
+                        ScreenName="PermissionPage"
+                    }
+                }
+            }
+        };
     }
 }
