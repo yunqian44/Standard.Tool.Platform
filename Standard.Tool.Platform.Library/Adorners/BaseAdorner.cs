a0428350f1a1980a032463a77f6b0516cc5226c4
yunqian44 <yunqian8@live.com>
2023-03-22 20:50:11 +0800

    Pagination Button

---
+++ Standard.Tool.Platform.Library/Adorners/BaseAdorner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Standard.Tool.Platform.Library.Adorners
+{
+    public class BaseAdorner : Adorner
+    {
+        /// <summary>
+        /// 装饰器上的Visual集合
+        /// </summary>
+        protected virtual VisualCollection VisualCollection { get; set; }
+
+        protected override int VisualChildrenCount => VisualCollection.Count;
+
+        protected override Visual GetVisualChild(int index) => VisualCollection[index];
+
+        public BaseAdorner(UIElement element) : base(element)
+        {
+            VisualCollection = new VisualCollection(this);
+        }
+    }
+}
---
+++ Standard.Tool.Platform.Library/Converters/FindResourceConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Standard.Tool.Platform.Library.Converters
+{
+    public class FindResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Application.Current.TryFindResource(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
