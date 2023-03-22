9b0daf30800359dcde2ed106f02d5d37a829455b
yunqian44 <yunqian8@live.com>
2023-03-22 21:06:10 +0800

    Paging UserControl

--- Standard.Tool.Platform/UserControls/UserControlMenuItem.xaml.cs
+++ Standard.Tool.Platform/UserControls/UserControlMenuItem.xaml.cs
@@ -1,22 +1,9 @@
-﻿using Standard.Tool.Platform.Core;
-using Standard.Tool.Platform.Extension;
+﻿using Standard.Tool.Platform.Extension;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static Standard.Tool.Platform.MainWindow;
 
 namespace Standard.Tool.Platform.UserControls
 {
