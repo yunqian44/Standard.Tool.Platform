55e34d49cdb055092718c6ea1227c0d943f7fc97
yunqian44 <yunqian8@live.com>
2023-03-22 20:50:46 +0800

    Permission Permission Query

--- Standard.Tool.Platform.Auth/PermissionFeature/GetPermissionsQuery.cs
+++ Standard.Tool.Platform.Auth/PermissionFeature/GetPermissionsQuery.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using NPOI.POIFS.Properties;
+using Org.BouncyCastle.Asn1.X509;
 using Standard.Tool.Platform.Common.Helper;
 using Standard.Tool.Platform.Data.Entities;
 using Standard.Tool.Platform.Data.Infrastructure;
@@ -13,7 +14,17 @@ using System.Threading.Tasks;
 
 namespace Standard.Tool.Platform.Auth.PermissionFeature;
 
-public record GetPermissionsQuery : IRequest<IList<Permission>>;
+public record GetPermissionsQuery : IRequest<IList<Permission>>
+{
+    public int PageSize { get; set; }
+    public int PageIndex { get; set; }
+
+    public GetPermissionsQuery(int pageIndex, int pageSize)
+    {
+        PageSize = pageSize;
+        PageIndex = pageIndex;
+    }
+}
 
 public class GetPermissionsQueryHandler : IRequestHandler<GetPermissionsQuery, IList<Permission>>
 {
@@ -23,33 +34,50 @@ public class GetPermissionsQueryHandler : IRequestHandler<GetPermissionsQuery, I
 
     public async Task<IList<Permission>> Handle(GetPermissionsQuery request, CancellationToken ct)
     {
-        var data = await _repo.SelectAsync((p) => new Permission
+        if (request.PageSize < 1)
         {
-            Id = p.Id.ToString().Trim(),
-            //IsShow = true,
-            CreateTimeUtc = p.CreateTimeUtc,
-            Code = p.Code,
-            Name = p.Name,
-            Type = p.Type,
-            TypeName = p.Type.GetDescription(),
-            ParentId = p.ParentId,
-            Status = p.Status,
-            No = 0,
-            Parent = new Permission(p.Parent),
-            LastModifiedTimeUtc = p.LastModifiedTimeUtc,
-            Childrens = p.Childrens.Select(sm => new Permission
-            {
-                Id = sm.Id.ToString().Trim(),
-                Code = sm.Code,
-                Name = sm.Name,
-                Type = sm.Type,
-                TypeName = sm.Type.GetDescription(),
-                ParentId = sm.ParentId,
-                Status = sm.Status,
-                CreateTimeUtc = sm.CreateTimeUtc,
-                LastModifiedTimeUtc = sm.LastModifiedTimeUtc,
-            }).ToArray()
-        }, ct); ;  
-        return data;
+            throw new ArgumentOutOfRangeException(nameof(request.PageSize),
+                $"{nameof(request.PageSize)} can not be less than 1, current value: {request.PageSize}.");
+        }
+
+        if (request.PageIndex < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(request.PageIndex),
+                $"{nameof(request.PageIndex)} can not be less than 1, current value: {request.PageIndex}.");
+        }
+
+        var spec = new PermissionPagingSpec(request.PageSize, request.PageIndex);
+        return await _repo.SelectAsync(spec, Permission.EntitySelector);
+
+        #region 暂时注释
+        //var data = await _repo.SelectAsync((p) => new Permission
+        //{
+        //    Id = p.Id.ToString().Trim(),
+        //    //IsShow = true,
+        //    CreateTimeUtc = p.CreateTimeUtc,
+        //    Code = p.Code,
+        //    Name = p.Name,
+        //    Type = p.Type,
+        //    TypeName = p.Type.GetDescription(),
+        //    ParentId = p.ParentId,
+        //    Status = p.Status,
+        //    No = 0,
+        //    Parent = new Permission(p.Parent),
+        //    LastModifiedTimeUtc = p.LastModifiedTimeUtc,
+        //    Childrens = p.Childrens.Select(sm => new Permission
+        //    {
+        //        Id = sm.Id.ToString().Trim(),
+        //        Code = sm.Code,
+        //        Name = sm.Name,
+        //        Type = sm.Type,
+        //        TypeName = sm.Type.GetDescription(),
+        //        ParentId = sm.ParentId,
+        //        Status = sm.Status,
+        //        CreateTimeUtc = sm.CreateTimeUtc,
+        //        LastModifiedTimeUtc = sm.LastModifiedTimeUtc,
+        //    }).ToArray()
+        //}, ct); 
+        //return data; 
+        #endregion
     }
 }
