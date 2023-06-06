c848013694e9e8732fdc3af5a1f08c38a4080916
yunqian44 <yunqian8@live.com>
2023-06-06 13:47:41 +0800

    Orcale EF Repository

---
+++ Standard.Tool.Platform.Data/Infrastructure/Oracle/IOracleRepository.cs
@@ -0,0 +1,56 @@
+﻿using MathNet.Numerics.Statistics.Mcmc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Data.Infrastructure
+{
+    public interface IOrcaleRepository<T> where T : class
+    {
+        Task CreateDBAsync(CancellationToken ct = default);
+        Task Clear(CancellationToken ct = default);
+
+        ValueTask<T> GetAsync(object key, CancellationToken ct = default);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> condition);
+
+        Task<IReadOnlyList<T>> ListAsync(CancellationToken ct = default);
+
+        IQueryable<T> AsQueryable();
+
+        Task<int> DeleteAsync(T entity, CancellationToken ct = default);
+
+        Task<int> DeleteAsync(CancellationToken ct = default);
+
+        Task DeleteAsync(IEnumerable<T> entities, CancellationToken ct = default);
+
+        Task<int> DeleteAsync(object key, CancellationToken ct = default);
+
+        Task<int> CountAsync(ISpecification<T> spec = null, CancellationToken ct = default);
+
+        Task<int> CountAsync(Expression<Func<T, bool>> condition, CancellationToken ct = default);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> condition = null, CancellationToken ct = default);
+
+        Task<IList<TResult>> SelectAsync<TResult>(Expression<Func<T, TResult>> selector, CancellationToken ct = default);
+
+        Task<IList<TResult>> SelectAsync<TResult>(ISpecification<T> spec, Expression<Func<T, TResult>> selector);
+
+        Task<TResult> FirstOrDefaultAsync<TResult>(ISpecification<T> spec, Expression<Func<T, TResult>> selector);
+
+        IList<TResult> Select<TResult>(Expression<Func<T, TResult>> selector);
+
+        Task<T> AddAsync(T entity, CancellationToken ct = default);
+
+        Task<int> UpdateAsync(T entity, CancellationToken ct = default);
+
+        int Update(T entity);
+
+        Task<int> AddRangeAsync(IEnumerable<T> entity);
+    }
+}
---
+++ Standard.Tool.Platform.Data/Infrastructure/Oracle/OracleDbContextRepository.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Data.Infrastructure
+{
+    public abstract class OrcaleDbContextRepository<T> : IOrcaleRepository<T> where T : class
+    {
+        protected readonly DbContext DbContext;
+
+        protected OrcaleDbContextRepository(DbContext ctx) => DbContext = ctx;
+
+
+        public async Task CreateDBAsync(CancellationToken ct = default)
+        {
+            bool canConnect = DbContext.Database.CanConnect();
+            if (!canConnect) return;
+
+            await DbContext.Database.EnsureCreatedAsync();
+        }
+
+        public Task Clear(CancellationToken ct = default)
+        {
+            DbContext.RemoveRange(DbContext.Set<T>());
+            return DbContext.SaveChangesAsync(ct);
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> condition) =>
+            DbContext.Set<T>().FirstOrDefaultAsync(condition);
+
+        public virtual ValueTask<T> GetAsync(object key, CancellationToken ct = default) =>
+            DbContext.Set<T>().FindAsync(keyValues: new[] { key }, cancellationToken: ct);
+
+        public async Task<IReadOnlyList<T>> ListAsync(CancellationToken ct = default) =>
+            await DbContext.Set<T>().AsNoTracking().ToListAsync(cancellationToken: ct);
+
+        public IQueryable<T> AsQueryable() => DbContext.Set<T>();
+
+        public async Task<int> DeleteAsync(T entity, CancellationToken ct = default)
+        {
+            DbContext.Set<T>().Remove(entity);
+            return await DbContext.SaveChangesAsync(ct);
+        }
+
+        public Task DeleteAsync(IEnumerable<T> entities, CancellationToken ct = default)
+        {
+            DbContext.Set<T>().RemoveRange(entities);
+            return DbContext.SaveChangesAsync(ct);
+        }
+
+
+        public Task<int> DeleteAsync(CancellationToken ct = default)
+        {
+            DbContext.Set<T>().RemoveRange();
+            return DbContext.SaveChangesAsync(ct);
+        }
+
+        public async Task<int> DeleteAsync(object key, CancellationToken ct = default)
+        {
+            var entity = await GetAsync(key, ct);
+            if (entity is not null)
+                return await DeleteAsync(entity, ct);
+            return -1;
+        }
+
+        public Task<int> CountAsync(Expression<Func<T, bool>> condition, CancellationToken ct = default) =>
+            DbContext.Set<T>().CountAsync(condition, ct);
+
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> condition = null, CancellationToken ct = default) =>
+            null != condition ?
+                DbContext.Set<T>().AnyAsync(condition, cancellationToken: ct) :
+                DbContext.Set<T>().AnyAsync(cancellationToken: ct);
+
+        public async Task<IList<TResult>> SelectAsync<TResult>(Expression<Func<T, TResult>> selector, CancellationToken ct = default) =>
+            await DbContext.Set<T>().Select(selector).ToListAsync(cancellationToken: ct);
+
+        public async Task<IList<TResult>> SelectAsync<TResult>(
+    ISpecification<T> spec, Expression<Func<T, TResult>> selector) =>
+    await ApplySpecification(spec).Select(selector).ToListAsync();
+
+        public async Task<TResult> FirstOrDefaultAsync<TResult>(
+        ISpecification<T> spec, Expression<Func<T, TResult>> selector) =>
+       await ApplySpecification(spec).AsNoTracking().Select(selector).FirstOrDefaultAsync();
+
+        public IList<TResult> Select<TResult>(Expression<Func<T, TResult>> selector) =>
+             DbContext.Set<T>().AsNoTracking().Select(selector).ToList();
+
+        public async Task<T> AddAsync(T entity, CancellationToken ct = default)
+        {
+            var resultStatus = await DbContext.SaveChangesAsync(ct);
+
+            return resultStatus == 1 ? entity : null;
+        }
+
+        public async Task<int> UpdateAsync(T entity, CancellationToken ct = default)
+        {
+            DbContext.Entry(entity).State = EntityState.Modified;
+            return await DbContext.SaveChangesAsync(ct);
+        }
+
+        public int Update(T entity)
+        {
+            DbContext.Entry(entity).State = EntityState.Modified;
+            return DbContext.SaveChanges();
+        }
+
+        public async Task<int> AddRangeAsync(IEnumerable<T> entityList)
+        {
+            await DbContext.Set<T>().AddRangeAsync(entityList);
+            return await DbContext.SaveChangesAsync();
+        }
+
+        public Task<int> CountAsync(ISpecification<T> spec = null, CancellationToken ct = default) =>
+            null != spec ? ApplySpecification(spec).CountAsync(cancellationToken: ct) : DbContext.Set<T>().CountAsync(cancellationToken: ct);
+
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec) => SpecificationEvaluator<T>.GetQuery(DbContext.Set<T>().AsQueryable(), spec);
+
+    }
+}
