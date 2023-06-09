01967f0370d640630bfecfb0dade48626c80da87
yunqian44 <yunqian8@live.com>
2023-06-10 02:00:33 +0800

    Cancel Windows Max

--- Standard.Tool.Platform/App.xaml.cs
+++ Standard.Tool.Platform/App.xaml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Windows;
 using Standard.Tool.Platform.Data.Infrastructure;
 using Standard.Tool.Platform.PostgreSql;
+using Standard.Tool.Platform.Oracle;
 using Standard.Tool.Platform.Data.Infrastructure.Services;
 using MediatR;
 using Standard.Tool.Platform.CommonPage;
@@ -45,6 +46,7 @@ public partial class App : Application
 
         var configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).Add(new JsonConfigurationSource { Path = $"appsettings.json", Optional = false, ReloadOnChange = true }).Build();
         var connStr = configuration.GetConnectionString("Standard.Tool.Platform.Database");
+        var connOracleStr = configuration.GetConnectionString("Oracle");
 
         var hostBuilder = Host.CreateDefaultBuilder(args);
         hostBuilder.ConfigureServices((ctx, services) =>
@@ -52,7 +54,10 @@ public partial class App : Application
             services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
             services.AddTransient<IReadTableService, ReadTableService>();
             services.AddSingleton(new TableDataHelper(AppDomain.CurrentDomain.BaseDirectory));
+
+            services.AddOracleStorage(connOracleStr);
             services.AddPostgreSqlStorage(connStr);
+            
 
             services.AddSingleton<MainWindow>();
             services.AddSingleton<AccountPage>();
