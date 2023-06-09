389c0eee67149e997fc848a2bd033acf7e59f8a5
yunqian44 <yunqian8@live.com>
2023-06-10 01:58:46 +0800

    Remove redundant code

--- Standard.Tool.Platform.PostgreSql/Infrastructure/PostgreSqlDbContextRepository.cs
+++ Standard.Tool.Platform.PostgreSql/Infrastructure/PostgreSqlDbContextRepository.cs
@@ -1,17 +1,11 @@
 ﻿using Standard.Tool.Platform.Data.Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Standard.Tool.Platform.PostgreSql.Infrastructure
+namespace Standard.Tool.Platform.PostgreSql.Infrastructure;
+
+public class PostgreSqlDbContextRepository<T> : DbContextRepository<T> where T : class
 {
-    public class PostgreSqlDbContextRepository<T> : DbContextRepository<T> where T : class
+    public PostgreSqlDbContextRepository(PostgreSqlToolsBlockDbContext dbContext)
+        : base(dbContext)
     {
-        public PostgreSqlDbContextRepository(PostgreSqlToolsBlockDbContext dbContext)
-            : base(dbContext)
-        {
-        }
     }
 }
