927765cb5932eb3209ea95922377d4173dd3f5af
yunqian44 <yunqian8@live.com>
2023-06-13 16:25:05 +0800

    Location Domain

---
+++ Standard.Tool.Platform.Locations/Location.cs
@@ -0,0 +1,59 @@
+using System;
+using System.ComponentModel;
+
+namespace Standard.Tool.Platform.Locations
+{
+    public class Location
+    {
+        [Description("序号")]
+        public int No { get; set; }
+
+
+        [Description("是否选中")]
+        public bool IsSelected { get; set; }
+
+        [Description("编码")]
+        public string CodeEmpl { get; set; }
+
+        [Description("名称")]
+        public string LibEmpl { get; set; }
+
+        public string EntitesStockees { get; set; }
+
+        [Description("总容量: --散货中没有 0")]   
+        public string NbEntites { get; set; }
+        
+        [Description("存储类型: 散货中没有 ''")]
+        public string TypEntites { get; set; }
+       
+        public string CapaTotaleEnP1 { get; set; }
+        
+        public string CapaRestanteEnP1 { get; set; }
+        
+        public string CapaReserveeEnP1 { get; set; } = "0";
+
+        [Description("是否散货")]
+        public string StockMasse { get; set; }
+
+        [Description("冷藏")]
+        public string ChambreFroide { get; set; }
+
+        [Description("安全")]
+        public string Stupefiant { get; set; }
+        //'0', 高度
+        public string HauteurSol { get; set; }
+        //' ', 列表
+        public string Liste { get; set; } = " ";
+        //'Y', 位置签署
+        public string Affectable { get; set; }
+        //'100', 重量约束 --散货中没有   -1
+        public string MasseMax { get; set; }
+        //'100', 高度限制 --散货中没有    -1  
+        public string HautMax { get; set; }
+        //'Y', 单批次     --散货中没有 N
+        public string MonoLot { get; set; }
+
+        //'N')状态 N正常 
+        public string Type { get; set; } = "N";
+    }
+}
---
+++ Standard.Tool.Platform.Locations/Validations/LocationValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Locations.Validations
+{
+    public abstract class LocationValidation<T> : AbstractValidator<T> where T : EditLocationRequest
+    {
+        //验证集合数量
+        protected void ValidateCollectionCount()
+        {
+            RuleFor(c => c.LocationDataList)
+                .NotEmpty()
+                .Must(HaveCollectionCount)//Must 表示必须满足某一个条件，参数是一个bool类型的方法，更像是一个委托事件
+                .WithMessage("集合数量必须大于0");
+        }
+
+        // 表达式
+        protected static bool HaveCollectionCount(IEnumerable<Location> locationData)
+        {
+            return locationData.Count() > 0;
+        }
+    }
+}
