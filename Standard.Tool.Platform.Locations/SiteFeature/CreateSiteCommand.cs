d1af9b0b18e9c86265181a4a9017df1b450b6f4f
yunqian44 <yunqian8@live.com>
2023-06-14 11:19:14 +0800

    Add SiteCommand

---
+++ Standard.Tool.Platform.Locations/SiteFeature/CreateSiteCommand.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Org.BouncyCastle.Asn1.X509;
+using SixLabors.Fonts.Tables.AdvancedTypographic;
+using Standard.Tool.Platform.Data.Entities.Project;
+using Standard.Tool.Platform.Data.Infrastructure;
+using Standard.Tool.Platform.Data.Spec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Locations.SiteFeature
+{
+    public record CreateSiteCommand(string Name) : IRequest<Site>;
+
+    public class CreateSiteCommandHandler : IRequestHandler<CreateSiteCommand, Site>
+    {
+        private readonly IRepository<SiteEntity> _repo;
+
+        public CreateSiteCommandHandler(IRepository<SiteEntity> repo) => _repo = repo;
+
+        public async Task<Site> Handle(CreateSiteCommand request, CancellationToken ct)
+        {
+            if (!Site.ValidateName(request.Name)) return null;
+
+
+            if (await _repo.AnyAsync(t => t.SiteName == request.Name, ct))
+            {
+                return await _repo.FirstOrDefaultAsync(new SiteSpec(request.Name), Site.EntitySelector);
+            }
+
+            var newSite = new SiteEntity
+            {
+                SiteName = request.Name
+            };
+
+            var site = await _repo.AddAsync(newSite, ct);
+
+            return new()
+            {
+                Name = site.SiteName
+            };
+        }
+    }
+}
---
+++ Standard.Tool.Platform.Locations/SiteFeature/Site.cs
@@ -0,0 +1,30 @@
+﻿using SixLabors.Fonts.Tables.AdvancedTypographic;
+using Standard.Tool.Platform.Data.Entities.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Standard.Tool.Platform.Locations.SiteFeature
+{
+    public class Site
+    {
+        public string Name { get; set; }
+
+        public static readonly Expression<Func<SiteEntity, Site>> EntitySelector = t => new()
+        {
+            Name=t.SiteName
+        };
+
+        public static bool ValidateName(string tagDisplayName)
+        {
+            if (string.IsNullOrWhiteSpace(tagDisplayName)) return false;
+
+            const string pattern = @"[\u4e00-\u9fa5]";
+            return !Regex.IsMatch(tagDisplayName, pattern);
+        }
+    }
+}
